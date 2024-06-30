7fb2ec8d43f34f5bb8179c3323dec7149c1d866b
Kazmer GECZI-KULCSAR <geczi-kulcsar.kazmer@amplio.hu>
2024-06-30 13:58:48 +0200

    removed unnecessary lines

--- speaker-controller.sh
+++ speaker-controller.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo ""
-echo "Press CTRL+B, D to detach!"
+echo "Press Ctrl+b, d to detach!"
 sleep 3
 docker exec -ti tidal-connect /usr/bin/tmux a -t speaker_controller_application
